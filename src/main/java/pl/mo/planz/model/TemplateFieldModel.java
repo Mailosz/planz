4e5d8dfcc24d9dd318f75b4aadbe54001fcbcab2
Milosz.Osadczuk <milosz.osadczuk@comarch.pl>
2023-11-07 16:39:16 +0100

    Poprawka zmian templejta

--- src/main/java/pl/mo/planz/model/TemplateFieldModel.java
+++ src/main/java/pl/mo/planz/model/TemplateFieldModel.java
@@ -1,15 +1,19 @@
 package pl.mo.planz.model;
 
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -57,4 +61,9 @@ public class TemplateFieldModel {
     @Getter
     @Setter
     boolean isPublic = true;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @Getter
+    @Setter
+    List<FieldValueModel> values;
 }
